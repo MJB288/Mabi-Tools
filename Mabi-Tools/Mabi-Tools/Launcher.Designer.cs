96e38c1dcbb05e2e78b908820df83996ac0328e8
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-06-29 22:36:19 -0400

    Initial Project with two forms. Also some more adjustments to the Readme

---
+++ Mabi-Tools/Mabi-Tools/Launcher.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Mabi_Tools
+{
+    partial class MainLauncher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCommerce = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCommerce
+            // 
+            this.btnCommerce.Location = new System.Drawing.Point(70, 12);
+            this.btnCommerce.Name = "btnCommerce";
+            this.btnCommerce.Size = new System.Drawing.Size(109, 47);
+            this.btnCommerce.TabIndex = 0;
+            this.btnCommerce.Text = "Commerce Calculator";
+            this.btnCommerce.UseVisualStyleBackColor = true;
+            this.btnCommerce.Click += new System.EventHandler(this.btnCommerce_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(70, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Commerce Calculator";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(202, 297);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(37, 13);
+            this.lblVersion.TabIndex = 2;
+            this.lblVersion.Text = "v0.0.1";
+            // 
+            // MainLauncher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 319);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCommerce);
+            this.Name = "MainLauncher";
+            this.Text = "Mabi-Tools";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCommerce;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblVersion;
+    }
+}
+
--- README.md
+++ README.md
@@ -9,14 +9,18 @@ I do have some standards for what kind of tools I would create.
 
 
 ## Commerce Calculator
-This was a suggestion from a friend. In the game, there is a commerce system that allows the player to transport goods between cities. The player pays for the goods they transport, and are paid according to value of the goods at the city they arrive. Players may chose any city to drop off their goods
+This was a suggestion from a friend. In the game, there is a commerce system that allows the player to transport goods between cities. The player pays for the goods they transport, and are paid according to value of the goods at the city they arrive. Players may choose any city to drop off their goods. Players also know the prices of a particular good in all of the other cities when selecting which goods to transport.
 
 There are two main factors that affect price:
 
 1. The further the distance, the higher the price.
-2. The more players that trade a certain good to a city, the value of the good temporarily lowers.
+2. The more players that trade a certain good to a city, the value of the good temporarily lowers in that city.
 
 Because of point 2, there is no one single good trading route, the best route dynamically changes in accordance to the behavior of all the players in the current channel of the server.
 
-Therefore
+Therefore, the idea behind this tool is to allow the player to quickly input the prices of a desired good and determine the best options. For the time being, I am to display:
 
+1. The net profit at each city
+2. Destination with the largest Profit
+3. Destination with the Best time to money ratio
+4. 
