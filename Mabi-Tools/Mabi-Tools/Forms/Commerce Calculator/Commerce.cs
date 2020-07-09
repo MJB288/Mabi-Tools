3040eba0b766d876ca31a4c5299ae5e037ddd0e1
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-07-08 20:53:14 -0400

    Added capitals to some of the variables

--- Mabi-Tools/Mabi-Tools/Forms/Commerce Calculator/Commerce.cs
+++ Mabi-Tools/Mabi-Tools/Forms/Commerce Calculator/Commerce.cs
@@ -17,11 +17,11 @@ namespace Mabi_Tools
         private readonly char SECONDARY_TEXT_SEPARATOR = ';';
         //Remembers the actively selected index for each checklist box.
         //Adjust these values to change the default selected value. If I make a configure file, I could add an option to change the default
-        private int clboxprevSelectedG = 0, clboxprevSelectedT = 0;
+        private int ClboxprevSelectedG = 0, ClboxprevSelectedT = 0;
         //These are for tracking the weight and slot capacity of the currently selected good
-        private int selectedGoodWeight = 0, selectedGoodSlots = 0;
+        private int SelectedGoodWeight = 0, SelectedGoodSlots = 0;
         //These are for tracking the current Weight Capacity and Slots of the transport
-        private int selectedTransportWeight = 0, selectedTransportSlots = 0;
+        private int SelectedTransportWeight = 0, SelectedTransportSlots = 0;
         private Dictionary<String, City> CityData;
         private Dictionary<String, Transport> TransportData;
         //Keeping this in global memory for filter purposes
@@ -81,9 +81,9 @@ namespace Mabi_Tools
 
             //Check off the default value in the lists when loading
             
-            clboxCities.SetItemChecked(this.clboxprevSelectedT, true);
-            clboxCities.SelectedItem = clboxCities.Items[clboxprevSelectedG];
-            UIHelper.populateGoodCheckListBox(clboxGoods, CityData[CityData.Keys.ToList()[this.clboxprevSelectedT]], clboxprevSelectedG);
+            clboxCities.SetItemChecked(this.ClboxprevSelectedT, true);
+            clboxCities.SelectedItem = clboxCities.Items[ClboxprevSelectedG];
+            UIHelper.populateGoodCheckListBox(clboxGoods, CityData[CityData.Keys.ToList()[this.ClboxprevSelectedT]], ClboxprevSelectedG);
             
             //Check off the first one for now.
             flpTransport.Controls.OfType<RadioButton>().First().Checked = true;
@@ -92,23 +92,23 @@ namespace Mabi_Tools
 
         private void clboxGoods_SelectedIndexChanged(object sender, EventArgs e)
         {
-            clboxprevSelectedG = UIHelper.makeListBoxExclusitivity(clboxGoods, clboxprevSelectedG);
+            ClboxprevSelectedG = UIHelper.makeListBoxExclusitivity(clboxGoods, ClboxprevSelectedG);
             lblTest.Text = clboxGoods.SelectedItem.ToString();
             //Now pull up the Weight And Slot statistics
-            selectedGoodSlots = CityData[clboxCities.SelectedItem.ToString()].goods[clboxprevSelectedG].slotCapacity;
-            selectedGoodWeight = CityData[clboxCities.SelectedItem.ToString()].goods[clboxprevSelectedG].weight;
-            lblItemWeight.Text = "Weight : " + selectedGoodWeight;
-            lblItemSlots.Text = "Slot Capacity : " + selectedGoodSlots;
+            SelectedGoodSlots = CityData[clboxCities.SelectedItem.ToString()].goods[ClboxprevSelectedG].slotCapacity;
+            SelectedGoodWeight = CityData[clboxCities.SelectedItem.ToString()].goods[ClboxprevSelectedG].weight;
+            lblItemWeight.Text = "Weight : " + SelectedGoodWeight;
+            lblItemSlots.Text = "Slot Capacity : " + SelectedGoodSlots;
         }
 
         private void clboxCities_SelectedIndexChanged(object sender, EventArgs e)
         {
             //Adjust as necessary 
-            adjustLabelsCities(clboxprevSelectedT, clboxCities.SelectedIndex);
-            clboxprevSelectedT = UIHelper.makeListBoxExclusitivity(clboxCities, clboxprevSelectedT);
+            adjustLabelsCities(ClboxprevSelectedT, clboxCities.SelectedIndex);
+            ClboxprevSelectedT = UIHelper.makeListBoxExclusitivity(clboxCities, ClboxprevSelectedT);
             lblTest.Text = clboxCities.SelectedItem.ToString();
             //Now adjust the Goods Box based off of the currently selected city
-            UIHelper.populateGoodCheckListBox(clboxGoods, CityData[clboxCities.SelectedItem.ToString()], clboxprevSelectedG);
+            UIHelper.populateGoodCheckListBox(clboxGoods, CityData[clboxCities.SelectedItem.ToString()], ClboxprevSelectedG);
 
         }
 
@@ -174,41 +174,6 @@ namespace Mabi_Tools
 
         //Methods for loading data from files
         //-------------------------------------------------------------------------------------------------
-        private void loadTransportData(String transportFile)
-        {
-            TransportData = new Dictionary<String, Transport>();
-            //Moving this variable out for Error Reporting
-            String name = "";
-            //Will be expanded upon later - for now only need name of transports
-            try
-            { 
-                string[] rawdata = File.ReadAllLines(transportFile);
-                foreach (String s in rawdata)
-                {
-                    //This could be condensed into one line of code, saving a bit of memory, but for Readability purposes I decided against that
-                    /*int graveI = s.IndexOf(MAIN_TEXT_SEPARATOR);
-                    int semiI = s.IndexOf(";");
-                    name = s.Substring(0, graveI);
-                    String slots = s.Substring(graveI + 1, semiI - graveI - 1);
-                    String weight = s.Substring(semiI + 1, s.Length - semiI - 1);*/
-                    String[] split = s.Split('`');
-                    name = split[0];
-                    //Current ordering assumes number of slots comes first and weight second
-                    Transport temp = new Transport(name, Int32.Parse(split[1]), Int32.Parse(split[2]));
-                    TransportData[name] = temp;
-                }
-            }
-            catch (FileNotFoundException ex)
-            {
-                MessageBox.Show("Error while loading Transport Data : \n" + ex.Message, "Error");
-                this.Close();
-            }
-            catch(FormatException fex)
-            {
-                MessageBox.Show("Error formatting Transport Data for '" + name  + "' : \n" + fex.Message, "Error");
-            }
-             
-        }
 
         private void cboxCommerce_CheckedChanged(object sender, EventArgs e)
         {
@@ -217,18 +182,18 @@ namespace Mabi_Tools
             { 
                 //The alpaca bonus cannot stack with the commerce bonus.
                 cboxAlpaca.Checked = false;
-                selectedTransportSlots += 1;
-                selectedTransportWeight += 100;
+                SelectedTransportSlots += 1;
+                SelectedTransportWeight += 100;
             }
             else
             {
-                selectedTransportSlots -= 1;
-                selectedTransportWeight -= 100;
+                SelectedTransportSlots -= 1;
+                SelectedTransportWeight -= 100;
             }
 
 
-            lblTransportSlots.Text = "Slots : " + selectedTransportSlots;
-            lblTransportWeight.Text = "Weight Capacity : " + selectedTransportWeight;
+            lblTransportSlots.Text = "Slots : " + SelectedTransportSlots;
+            lblTransportWeight.Text = "Weight Capacity : " + SelectedTransportWeight;
         }
 
         private void tsmExit_Click(object sender, EventArgs e)
@@ -248,31 +213,31 @@ namespace Mabi_Tools
             if (cboxAlpaca.Checked)
             {
                 cboxCommerce.Checked = false;
-                selectedTransportSlots += 2;
-                selectedTransportWeight += 200;
+                SelectedTransportSlots += 2;
+                SelectedTransportWeight += 200;
 
             }
             else
             {
-                selectedTransportSlots -= 2;
-                selectedTransportWeight -= 200;
+                SelectedTransportSlots -= 2;
+                SelectedTransportWeight -= 200;
             }
-            lblTransportSlots.Text = "Slots : " + selectedTransportSlots;
-            lblTransportWeight.Text = "Weight Capacity : " + selectedTransportWeight;
+            lblTransportSlots.Text = "Slots : " + SelectedTransportSlots;
+            lblTransportWeight.Text = "Weight Capacity : " + SelectedTransportWeight;
         }
 
         public void rbtnTransport_CheckedChanged(object sender, EventArgs e)
         {
             //The RadioButton Text should be exactly the same as the indexes used in the Dictionary. An alternative would be using the number found at the end of each radiobutton name
             RadioButton selectedRBTN = flpTransport.Controls.OfType<RadioButton>().FirstOrDefault(rbtn => rbtn.Checked);
-            selectedTransportSlots = TransportData[selectedRBTN.Text].slots;
-            selectedTransportWeight = TransportData[selectedRBTN.Text].weight;
+            SelectedTransportSlots = TransportData[selectedRBTN.Text].slots;
+            SelectedTransportWeight = TransportData[selectedRBTN.Text].weight;
            
             //If the player has a commerce partner - they get a small boost to weight capacity and slots
             if (cboxCommerce.Checked)
             {
-                selectedTransportSlots += 1;
-                selectedTransportWeight += 100;
+                SelectedTransportSlots += 1;
+                SelectedTransportWeight += 100;
             }
             //Enable the player to check the Alpaca bonus
             if(selectedRBTN.Text.Equals("Wagon"))
@@ -285,8 +250,8 @@ namespace Mabi_Tools
                 cboxAlpaca.Checked = false;
                 cboxAlpaca.Enabled = false;
             }
-            lblTransportSlots.Text = "Slots : " + selectedTransportSlots;
-            lblTransportWeight.Text = "Weight Capacity : " + selectedTransportWeight;
+            lblTransportSlots.Text = "Slots : " + SelectedTransportSlots;
+            lblTransportWeight.Text = "Weight Capacity : " + SelectedTransportWeight;
         }
 
         private void btnCompute_Click(object sender, EventArgs e)
@@ -297,15 +262,15 @@ namespace Mabi_Tools
             int numGoods = 0;
             //Calculate the total number of goods
             //First - assume we can fill the toal number of slots
-            int ongoingWeight = selectedGoodWeight * selectedGoodSlots * selectedTransportSlots;
+            int ongoingWeight = SelectedGoodWeight * SelectedGoodSlots * SelectedTransportSlots;
             //Now check if we're over the weight limit - if yes then reduce the number of goods
-            if(ongoingWeight > selectedTransportWeight)
+            if(ongoingWeight > SelectedTransportWeight)
             {
-                numGoods = selectedTransportWeight / selectedGoodWeight;
+                numGoods = SelectedTransportWeight / SelectedGoodWeight;
             }
             else
             {
-                numGoods = selectedGoodSlots * selectedTransportSlots;
+                numGoods = SelectedGoodSlots * SelectedTransportSlots;
             }
 
             //Loop Through all of the text boxes
