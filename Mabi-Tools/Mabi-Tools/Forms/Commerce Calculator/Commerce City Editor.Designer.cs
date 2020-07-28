3dd0ec7f454b2e4d3f254ac5d5e1719fa3b91f69
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-07-28 19:40:34 -0400

    Added a missing label to the City Editor. Also Greatly reduced negative space by making form more compact

--- Mabi-Tools/Mabi-Tools/Forms/Commerce Calculator/Commerce City Editor.Designer.cs
+++ Mabi-Tools/Mabi-Tools/Forms/Commerce Calculator/Commerce City Editor.Designer.cs
@@ -51,6 +51,7 @@
             this.btnMoveDownCity = new System.Windows.Forms.Button();
             this.btnMoveUpGood = new System.Windows.Forms.Button();
             this.btnMoveDownGood = new System.Windows.Forms.Button();
+            this.lblGood = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clboxCities
@@ -60,7 +61,7 @@
             this.clboxCities.Items.AddRange(new object[] {
             "Town1",
             "Town2"});
-            this.clboxCities.Location = new System.Drawing.Point(69, 91);
+            this.clboxCities.Location = new System.Drawing.Point(12, 53);
             this.clboxCities.Name = "clboxCities";
             this.clboxCities.Size = new System.Drawing.Size(137, 124);
             this.clboxCities.TabIndex = 0;
@@ -70,7 +71,7 @@
             // 
             this.lblWeight.AutoSize = true;
             this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblWeight.Location = new System.Drawing.Point(371, 279);
+            this.lblWeight.Location = new System.Drawing.Point(295, 216);
             this.lblWeight.Name = "lblWeight";
             this.lblWeight.Size = new System.Drawing.Size(75, 20);
             this.lblWeight.TabIndex = 2;
@@ -80,7 +81,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(348, 311);
+            this.label1.Location = new System.Drawing.Point(272, 248);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 20);
             this.label1.TabIndex = 3;
@@ -90,7 +91,7 @@
             // 
             this.lblCities.AutoSize = true;
             this.lblCities.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCities.Location = new System.Drawing.Point(108, 58);
+            this.lblCities.Location = new System.Drawing.Point(48, 20);
             this.lblCities.Name = "lblCities";
             this.lblCities.Size = new System.Drawing.Size(64, 20);
             this.lblCities.TabIndex = 4;
@@ -98,7 +99,7 @@
             // 
             // btnAddCity
             // 
-            this.btnAddCity.Location = new System.Drawing.Point(361, 168);
+            this.btnAddCity.Location = new System.Drawing.Point(285, 105);
             this.btnAddCity.Name = "btnAddCity";
             this.btnAddCity.Size = new System.Drawing.Size(89, 23);
             this.btnAddCity.TabIndex = 5;
@@ -108,7 +109,7 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(225, 100);
+            this.btnDelete.Location = new System.Drawing.Point(161, 62);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 6;
@@ -118,7 +119,7 @@
             // 
             // btnDeleteGood
             // 
-            this.btnDeleteGood.Location = new System.Drawing.Point(225, 262);
+            this.btnDeleteGood.Location = new System.Drawing.Point(161, 224);
             this.btnDeleteGood.Name = "btnDeleteGood";
             this.btnDeleteGood.Size = new System.Drawing.Size(75, 23);
             this.btnDeleteGood.TabIndex = 8;
@@ -128,14 +129,14 @@
             // 
             // txtGoodWeight
             // 
-            this.txtGoodWeight.Location = new System.Drawing.Point(493, 278);
+            this.txtGoodWeight.Location = new System.Drawing.Point(417, 215);
             this.txtGoodWeight.Name = "txtGoodWeight";
             this.txtGoodWeight.Size = new System.Drawing.Size(100, 20);
             this.txtGoodWeight.TabIndex = 9;
             // 
             // txtGoodSlots
             // 
-            this.txtGoodSlots.Location = new System.Drawing.Point(493, 313);
+            this.txtGoodSlots.Location = new System.Drawing.Point(417, 250);
             this.txtGoodSlots.Name = "txtGoodSlots";
             this.txtGoodSlots.Size = new System.Drawing.Size(100, 20);
             this.txtGoodSlots.TabIndex = 10;
@@ -144,7 +145,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(359, 131);
+            this.label2.Location = new System.Drawing.Point(283, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(100, 20);
             this.label2.TabIndex = 11;
@@ -152,14 +153,14 @@
             // 
             // txtCityName
             // 
-            this.txtCityName.Location = new System.Drawing.Point(504, 133);
+            this.txtCityName.Location = new System.Drawing.Point(417, 70);
             this.txtCityName.Name = "txtCityName";
             this.txtCityName.Size = new System.Drawing.Size(100, 20);
             this.txtCityName.TabIndex = 12;
             // 
             // btnEditCity
             // 
-            this.btnEditCity.Location = new System.Drawing.Point(504, 168);
+            this.btnEditCity.Location = new System.Drawing.Point(428, 105);
             this.btnEditCity.Name = "btnEditCity";
             this.btnEditCity.Size = new System.Drawing.Size(89, 23);
             this.btnEditCity.TabIndex = 13;
@@ -169,7 +170,7 @@
             // 
             // btnAddGood
             // 
-            this.btnAddGood.Location = new System.Drawing.Point(361, 339);
+            this.btnAddGood.Location = new System.Drawing.Point(285, 276);
             this.btnAddGood.Name = "btnAddGood";
             this.btnAddGood.Size = new System.Drawing.Size(89, 23);
             this.btnAddGood.TabIndex = 14;
@@ -179,7 +180,7 @@
             // 
             // btnEditGood
             // 
-            this.btnEditGood.Location = new System.Drawing.Point(504, 339);
+            this.btnEditGood.Location = new System.Drawing.Point(428, 276);
             this.btnEditGood.Name = "btnEditGood";
             this.btnEditGood.Size = new System.Drawing.Size(89, 23);
             this.btnEditGood.TabIndex = 15;
@@ -191,7 +192,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(357, 247);
+            this.label3.Location = new System.Drawing.Point(281, 184);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 20);
             this.label3.TabIndex = 16;
@@ -199,14 +200,14 @@
             // 
             // txtGoodName
             // 
-            this.txtGoodName.Location = new System.Drawing.Point(493, 247);
+            this.txtGoodName.Location = new System.Drawing.Point(417, 184);
             this.txtGoodName.Name = "txtGoodName";
             this.txtGoodName.Size = new System.Drawing.Size(100, 20);
             this.txtGoodName.TabIndex = 17;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(284, 397);
+            this.btnSave.Location = new System.Drawing.Point(176, 360);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(84, 41);
             this.btnSave.TabIndex = 18;
@@ -216,7 +217,7 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(389, 397);
+            this.btnCancel.Location = new System.Drawing.Point(281, 360);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(84, 41);
             this.btnCancel.TabIndex = 19;
@@ -231,7 +232,7 @@
             this.clboxGoods.Items.AddRange(new object[] {
             "Good1",
             "Good2"});
-            this.clboxGoods.Location = new System.Drawing.Point(69, 247);
+            this.clboxGoods.Location = new System.Drawing.Point(12, 209);
             this.clboxGoods.Name = "clboxGoods";
             this.clboxGoods.Size = new System.Drawing.Size(137, 124);
             this.clboxGoods.TabIndex = 20;
@@ -239,7 +240,7 @@
             // 
             // btnMoveUpCity
             // 
-            this.btnMoveUpCity.Location = new System.Drawing.Point(225, 134);
+            this.btnMoveUpCity.Location = new System.Drawing.Point(161, 96);
             this.btnMoveUpCity.Name = "btnMoveUpCity";
             this.btnMoveUpCity.Size = new System.Drawing.Size(75, 23);
             this.btnMoveUpCity.TabIndex = 21;
@@ -249,7 +250,7 @@
             // 
             // btnMoveDownCity
             // 
-            this.btnMoveDownCity.Location = new System.Drawing.Point(225, 168);
+            this.btnMoveDownCity.Location = new System.Drawing.Point(161, 130);
             this.btnMoveDownCity.Name = "btnMoveDownCity";
             this.btnMoveDownCity.Size = new System.Drawing.Size(75, 23);
             this.btnMoveDownCity.TabIndex = 22;
@@ -259,7 +260,7 @@
             // 
             // btnMoveUpGood
             // 
-            this.btnMoveUpGood.Location = new System.Drawing.Point(225, 295);
+            this.btnMoveUpGood.Location = new System.Drawing.Point(161, 257);
             this.btnMoveUpGood.Name = "btnMoveUpGood";
             this.btnMoveUpGood.Size = new System.Drawing.Size(75, 23);
             this.btnMoveUpGood.TabIndex = 23;
@@ -269,7 +270,7 @@
             // 
             // btnMoveDownGood
             // 
-            this.btnMoveDownGood.Location = new System.Drawing.Point(225, 330);
+            this.btnMoveDownGood.Location = new System.Drawing.Point(161, 292);
             this.btnMoveDownGood.Name = "btnMoveDownGood";
             this.btnMoveDownGood.Size = new System.Drawing.Size(75, 23);
             this.btnMoveDownGood.TabIndex = 24;
@@ -277,11 +278,22 @@
             this.btnMoveDownGood.UseVisualStyleBackColor = true;
             this.btnMoveDownGood.Click += new System.EventHandler(this.btnMoveDownGood_Click);
             // 
+            // lblGood
+            // 
+            this.lblGood.AutoSize = true;
+            this.lblGood.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGood.Location = new System.Drawing.Point(44, 186);
+            this.lblGood.Name = "lblGood";
+            this.lblGood.Size = new System.Drawing.Size(72, 20);
+            this.lblGood.TabIndex = 25;
+            this.lblGood.Text = "Goods :";
+            // 
             // frmCommerceCityEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(540, 408);
+            this.Controls.Add(this.lblGood);
             this.Controls.Add(this.btnMoveDownGood);
             this.Controls.Add(this.btnMoveUpGood);
             this.Controls.Add(this.btnMoveDownCity);
@@ -338,5 +350,6 @@
         private System.Windows.Forms.Button btnMoveDownCity;
         private System.Windows.Forms.Button btnMoveUpGood;
         private System.Windows.Forms.Button btnMoveDownGood;
+        private System.Windows.Forms.Label lblGood;
     }
 }
