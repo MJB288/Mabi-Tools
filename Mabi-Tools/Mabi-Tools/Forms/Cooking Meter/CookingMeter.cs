108afdeebff71480b74864fbb8f537019c3bb9f1
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-10-07 01:05:14 -0400

    Fixes Disappearing Graphics
    Finally solved it. Form is now Double Buffered, painting is now done through a paint event handler. Textbox input is now processed in a separate function. Percentages are now a member of the form for redrawing purposes.

--- Mabi-Tools/Mabi-Tools/Forms/Cooking Meter/CookingMeter.cs
+++ Mabi-Tools/Mabi-Tools/Forms/Cooking Meter/CookingMeter.cs
@@ -20,6 +20,8 @@ namespace Mabi_Tools.Forms.Cooking_Meter
         private static readonly int METER_HEIGHT = 5;
         private static readonly Color[] RECTANGLE_COLORS = { Color.Green, Color.Yellow, Color.Red };
         private Graphics FormGraphics;
+        private bool DrawRectangles = false;
+        private int[] Percentages = {30, 40, 30};
         public frmCooking()
         {
             InitializeComponent();
@@ -28,6 +30,7 @@ namespace Mabi_Tools.Forms.Cooking_Meter
 
         private void frmCooking_Load(object sender, EventArgs e)
         {
+            this.DoubleBuffered = true;
             this.TopMost = true;
             //draw3Rectangles();
             this.Paint += frmCooking_Paint;
@@ -35,13 +38,10 @@ namespace Mabi_Tools.Forms.Cooking_Meter
 
         private void frmCooking_Paint(object sender, PaintEventArgs pe)
         {
-            //draw3Rectangles();
+            draw3Rectangles(pe.Graphics);
         }
 
-        /// <summary>
-        /// Draws the the 3 rectangles that represent the percentages of the meter
-        /// </summary>
-        private void draw3Rectangles()
+        private int[] getTextBoxInput()
         {
             int[] percentages = { 0, 0, 0 };
             try
@@ -53,7 +53,7 @@ namespace Mabi_Tools.Forms.Cooking_Meter
                 catch (FormatException fex)
                 {
                     MessageBox.Show("Error: Could not parse integer from Ingredient 1!\n" + fex.Message, "Format Error");
-                    return;
+                    return null;
                 }
                 try
                 {
@@ -62,13 +62,13 @@ namespace Mabi_Tools.Forms.Cooking_Meter
                 catch (FormatException fex)
                 {
                     MessageBox.Show("Error: Could not parse integer from Ingredient 2!\n" + fex.Message, "Format Error");
-                    return;
+                    return null;
                 }
             }
             catch (OverflowException oex) //I trust my users to be responsible for this one instance - but I'll catch this anyway.
             {
                 MessageBox.Show("Error : Integer too large!\n" + oex.Message, "Overflow Error");
-                return;
+                return null;
             }
             //Sanitize User input
 
@@ -76,33 +76,40 @@ namespace Mabi_Tools.Forms.Cooking_Meter
             if (percentages[0] < 0 || percentages[1] < 0)
             {
                 MessageBox.Show("Error : Percentages must be a positve number!", "Incorrect Input");
-                return;
+                return null;
             }
             //Next - check that we don't go over 100%
             if (percentages[0] + percentages[1] > 100)
             {
                 MessageBox.Show("Error : The two percentages cannot be greater than 100!");
-                return;
+                return null;
             }
 
             //Calculate third portion
             percentages[2] = 100 - (percentages[0] + percentages[1]);
+            return percentages;
+        }
 
+        /// <summary>
+        /// Draws the the 3 rectangles that represent the percentages of the meter
+        /// </summary>
+        private void draw3Rectangles(Graphics graphics)
+        {
             float cur_x = METER_START_X, cur_y = METER_START_Y;
 
-            FormGraphics.Clear(Color.FromArgb(71, 69, 152));
+            //graphics.Clear(Color.FromArgb(71, 69, 152));
             
             //Now draw each rectangle
-            for (int i = 0; i < percentages.Length; i++)
+            for (int i = 0; i < Percentages.Length; i++)
             {
-                drawRectangleCooking((float)(.01 * percentages[i] * METER_LENGTH), cur_x, cur_y, RECTANGLE_COLORS[i]);
+                drawRectangleCooking((float)(.01 * Percentages[i] * METER_LENGTH), cur_x, cur_y, RECTANGLE_COLORS[i], graphics);
                 //Update Current X
-                cur_x += (float)(0.01 * percentages[i] * METER_LENGTH);
+                cur_x += (float)(0.01 * Percentages[i] * METER_LENGTH);
             }
             lblTest.Text = "";
-            for(int i = 0; i < percentages.Length; i++) 
+            for(int i = 0; i < Percentages.Length; i++) 
             {
-                lblTest.Text += " - " + 0.01 * percentages[i] * METER_LENGTH;
+                lblTest.Text += " - " + 0.01 * Percentages[i] * METER_LENGTH;
             }
             //Invalidate the region upon success
             /*Rectangle MeterRegion = new Rectangle(METER_START_X, METER_START_Y, METER_LENGTH, METER_HEIGHT);
@@ -112,19 +119,22 @@ namespace Mabi_Tools.Forms.Cooking_Meter
 
 
 
-        private void drawRectangleCooking(float length, float x, float y, Color colorType)
+        private void drawRectangleCooking(float length, float x, float y, Color colorType, Graphics graphics)
         {
             SolidBrush mainBrush = new SolidBrush(colorType);
             //Graphics formGraphics = this.CreateGraphics();
-            FormGraphics.SmoothingMode = SmoothingMode.HighQuality;
-            FormGraphics.FillRectangle(mainBrush, new RectangleF(x, y, length, METER_HEIGHT));
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.FillRectangle(mainBrush, new RectangleF(x, y, length, METER_HEIGHT));
             //formGraphics.Dispose();
             mainBrush.Dispose();
         }
 
         private void btnDisplay_Click(object sender, EventArgs e)
         {
-            draw3Rectangles();
+            Percentages = getTextBoxInput();
+            //pboxCMeter.Invalidate();
+            //DrawRectangles = true;
+            Invalidate();
         }
     }
 }
