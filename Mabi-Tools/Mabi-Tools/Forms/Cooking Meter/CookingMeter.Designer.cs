15c595871a9fb6b517c00375b7dcaecc26fb15d3
Guifindor <42903599+Guifindor@users.noreply.github.com>
2020-10-18 01:49:49 -0400

    Light Gray Background
    Gives the click-through properties, but doesn't mess with bright colors like darkgreen on the guidelines

--- Mabi-Tools/Mabi-Tools/Forms/Cooking Meter/CookingMeter.Designer.cs
+++ Mabi-Tools/Mabi-Tools/Forms/Cooking Meter/CookingMeter.Designer.cs
@@ -105,7 +105,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(285, 387);
             this.Controls.Add(this.btnGuideMode);
             this.Controls.Add(this.lblIng2);
@@ -116,7 +116,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmCooking";
             this.Text = "Cooking Meter";
-            this.TransparencyKey = System.Drawing.Color.DarkGreen;
+            this.TransparencyKey = System.Drawing.Color.LightGray;
             this.Load += new System.EventHandler(this.frmCooking_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
